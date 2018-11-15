294e7c40a46908a6735d29d7e343b52592cffc32
jerion <23549684+jerion@users.noreply.github.com>
2018-11-15 15:13:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/user/lab8/MyService.java
@@ -0,0 +1,43 @@
+package com.example.user.lab8;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        return null;
+    }
+
+    public void onCreate()
+    {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try
+                {
+                    Thread.sleep(50000);
+
+                    Intent intent = new Intent(MyService.this, Main2Activity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+                    MyService.this.startActivity(intent);
+                }catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+    }
+}
